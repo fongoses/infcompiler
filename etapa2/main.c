7846e97265588ab965519e3d785439fed49cd46a
Mario Gasparoni <mcgjunior@inf.ufrgs.br>
2013-04-30 19:17:35 -0300

    corrigido erro descrito pelo professor: nao reconhecia multiplas strings.

--- etapa2/main.c
+++ etapa2/main.c
@@ -19,7 +19,7 @@ extern int running;
 extern int yyparse(void);
 
 #define TESTE_MANUAL 0
-
+#define DEBUGGING 1
 
 
 int main(int argc , char ** argv){
@@ -28,20 +28,20 @@ int main(int argc , char ** argv){
 	if(!TESTE_MANUAL){
 
 		if(argc < 2) {
-                	fprintf(stderr,"Error: No input file\n");
+                	if(DEBUGGING) fprintf(stderr,"Error: No input file\n");
                 	exit(3);
        	 	}
 
         	yyin=fopen(argv[1],"r"); //yyin: global var which stores the file pointer to the current input file of the lexycal analyzer
 
         	if(yyin <= 0) {
-                	fprintf(stderr,"Error opening the file\n");
+                	if(DEBUGGING)fprintf(stderr,"Error opening the file\n");
                 	exit(3);
         	}
 		initMe();
 		yyparse();
 		//yyerror (in yacc.y) handles erros, exiting 3		
-		fprintf(stdout,"Compilation Successfull.\n");
+	 	if(DEBUGGING)fprintf(stdout,"Compilation Successfull.\n");
 		exit(0);
 
 	}else{
@@ -49,7 +49,7 @@ int main(int argc , char ** argv){
 		initMe();	
 		yyparse(); //if something goes wrong, then yyerror will be called and the program dies here.
 		
-		fprintf(stdout,"Compilation Successfull.\n");
+		if(DEBUGGING)fprintf(stdout,"Compilation Successfull.\n");
 		exit(0); //successfull compilation: exits with 0
 	}	
 
