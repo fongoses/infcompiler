91ee33edd7e07fcdff321a6f487fe077cec01694
Mario Gasparoni <mcgjunior@inf.ufrgs.br>
2013-05-18 01:36:10 -0300

    Script de teste melhorado

--- etapa2/test.sh
+++ etapa2/test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-title="Running tests...";
 accepted="ACCEPTED";
 rejected="REJECTED";
 result=0;
+TESTS_DIR=.;
+title="Running tests...";
 echo $title;
 
-for i in 1 2 3 4 5 6 7 8 9 10
+for i in $TESTS_DIR/*.ling # para todos arquivos .ling
 do
 
-	./etapa2 sample$i.ling;
+	./etapa2 $i 
 	result=$?; #resultado do ultimo programa executado
 	if [ $result == 0 ] 
 	then echo "Test $i: $accepted"
